895f7f7a83d0258615b7156c53b6c965d77e54a3
shotius <sh.archemashvili@gmail.com>
2021-03-29 10:38:38 +0400

    when loaded less then 10 questions, fixed

--- src/components/EndPage.js
+++ src/components/EndPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const EndPage = ({state, dispatch}) => {
+  const allQuestions = state.questions.results;
+
     return (
         <div className='result' >
             {
                 state.correctAnswers > 5
-                    ? <p className='success'>passed {state.correctAnswers}/10</p>
-                    : <p className='fail'>failed {state.correctAnswers}/10</p>
+                    ? <p className='success'>passed {state.correctAnswers}/{allQuestions.length}</p>
+                    : <p className='fail'>failed {state.correctAnswers}/{allQuestions.length}</p>
             }
             <button 
                  onClick={() => dispatch({type: 'start_again'})} 
